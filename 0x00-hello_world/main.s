4fa869fe34a9f454a6c7019812447720231baf6c
wac <wabaham9@gmail.com>
2022-06-30 17:05:38 +0000

    q0-8

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,22 +1,60 @@
 	.file	"main.c"
+	.intel_syntax noprefix
+# GNU C17 (Ubuntu 9.4.0-1ubuntu1~20.04.1) version 9.4.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 9.4.0, GMP version 6.2.0, MPFR version 4.0.2, MPC version 1.1.0, isl version isl-0.22.1-GMP
+
+# GGC heuristics: --param ggc-min-expand=96 --param ggc-min-heapsize=125009
+# options passed:  -imultiarch x86_64-linux-gnu main.c -masm=intel
+# -mtune=generic -march=x86-64 -Og -fverbose-asm
+# -fasynchronous-unwind-tables -fstack-protector-strong -Wformat
+# -Wformat-security -fstack-clash-protection -fcf-protection
+# options enabled:  -fPIC -fPIE -faggressive-loop-optimizations
+# -fassume-phsa -fasynchronous-unwind-tables -fauto-inc-dec
+# -fcombine-stack-adjustments -fcommon -fcompare-elim -fcprop-registers
+# -fdefer-pop -fdelete-null-pointer-checks -fdwarf2-cfi-asm
+# -fearly-inlining -feliminate-unused-debug-types -fforward-propagate
+# -ffp-int-builtin-inexact -ffunction-cse -fgcse-lm -fgnu-runtime
+# -fgnu-unique -fguess-branch-probability -fident -finline -finline-atomics
+# -fipa-profile -fipa-pure-const -fipa-reference
+# -fipa-reference-addressable -fipa-stack-alignment -fira-hoist-pressure
+# -fira-share-save-slots -fira-share-spill-slots -fivopts
+# -fkeep-static-consts -fleading-underscore -flifetime-dse
+# -flto-odr-type-merging -fmath-errno -fmerge-constants
+# -fmerge-debug-strings -fomit-frame-pointer -fpeephole -fplt
+# -fprefetch-loop-arrays -freg-struct-return -freorder-blocks
+# -fsched-critical-path-heuristic -fsched-dep-count-heuristic
+# -fsched-group-heuristic -fsched-interblock -fsched-last-insn-heuristic
+# -fsched-rank-heuristic -fsched-spec -fsched-spec-insn-heuristic
+# -fsched-stalled-insns-dep -fschedule-fusion -fsemantic-interposition
+# -fshow-column -fshrink-wrap -fshrink-wrap-separate -fsigned-zeros
+# -fsplit-ivs-in-unroller -fsplit-wide-types -fssa-backprop
+# -fstack-clash-protection -fstack-protector-strong -fstdarg-opt
+# -fstrict-volatile-bitfields -fsync-libcalls -ftoplevel-reorder
+# -ftrapping-math -ftree-builtin-call-dce -ftree-ccp -ftree-ch
+# -ftree-coalesce-vars -ftree-copy-prop -ftree-cselim -ftree-dce
+# -ftree-dominator-opts -ftree-dse -ftree-forwprop -ftree-fre
+# -ftree-loop-if-convert -ftree-loop-im -ftree-loop-ivcanon
+# -ftree-loop-optimize -ftree-parallelize-loops= -ftree-phiprop
+# -ftree-reassoc -ftree-scev-cprop -ftree-sink -ftree-slsr -ftree-ter
+# -funit-at-a-time -funwind-tables -fverbose-asm -fzero-initialized-in-bss
+# -m128bit-long-double -m64 -m80387 -malign-stringops
+# -mavx256-split-unaligned-load -mavx256-split-unaligned-store
+# -mfancy-math-387 -mfp-ret-in-387 -mfxsr -mglibc -mieee-fp
+# -mlong-double-80 -mmmx -mno-sse4 -mpush-args -mred-zone -msse -msse2
+# -mstv -mtls-direct-seg-refs -mvzeroupper
+
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB23:
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
+	endbr64	
+# main.c:11: }
+	mov	eax, 0	#,
+	ret	
 	.cfi_endproc
-.LFE0:
+.LFE23:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
