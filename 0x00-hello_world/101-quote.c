820b05c6af033cf90c86c2355fe80fd149aa7fe8
wac <wabaham9@gmail.com>
2022-06-30 19:45:51 +0000

    q8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return:1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, text, sizeof(ch));
 	return (1);
 }
