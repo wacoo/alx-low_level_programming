c72962995024f11c3a3a8d4330aef13806983f47
wac <wabaham9@gmail.com>
2022-07-06 18:20:45 +0000

    q0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -10,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
