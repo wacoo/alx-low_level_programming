78f1ba4d5d5947fbd92145d4a9e21996aa55eb25
wac <wabaham9@gmail.com>
2022-07-07 18:38:00 +0000

    last

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,13 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
+
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -35,4 +35,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-
