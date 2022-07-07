02e9506447b5a54308888bc18fa0f5ad889f0949
wac <wabaham9@gmail.com>
2022-07-07 12:29:59 +0000

    q9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,29 +15,27 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			if (i >= 100)
-			{
-				printf("%d", i);
-			}
-			else
-			{
-				printf("%d ", i);
-			}
-
+			printf("%d", i);
 		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
