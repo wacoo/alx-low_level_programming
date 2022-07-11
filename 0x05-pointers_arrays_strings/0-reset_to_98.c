a7c31ce707a23b660faeb4e34e7a1967757a472d
wac <wabaham9@gmail.com>
2022-07-11 09:59:01 +0000

    Q0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - changes a pointer value to 98
+ * @n: pointer whose value whould change
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains programs related to pointers, arrays and strings.
