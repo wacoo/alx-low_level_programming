fa394a65d53f79de24f8325e7439e9b7e2af828b
wac <wabaham9@gmail.com>
2022-07-14 12:37:26 +0000

    Q3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' || *s2 != '\0')
+	int i = 0;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (*s1 < *s2)
+		if (s1[i] < s2[i])
 		{
-			return (-15);
+			return (s1[i] - s2[i]);
 		}
-		else if (*s1 > *s2)
+		else if (s1[i] > s2[i])
 		{
-			return (15);
+			return (s1[i] - s2[i]);
 		}
-		s1++;
-		s2++;
+		i++;
 	}
 	return (0);
 }
