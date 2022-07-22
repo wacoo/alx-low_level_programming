ceaad8fa46d97a77a7114a2ff16717c0341fb0f3
wac <wabaham9@gmail.com>
2022-07-22 11:13:49 +0000

    q0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _isdigit - checks if a character is number
+ * @c: character
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
  * _atoi - extracts number from string
  * @s: string
  *
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,11 +3,10 @@
  * _putchar - prints a character
  * @c: the caracter to be printed
  *
- * Retrun: 0
+ * Return: success
  */
 int _putchar(char c)
 {
-	write(1, &c, 1);
-	return 0;
+	return (write(1, &c, 1));
 }
 
