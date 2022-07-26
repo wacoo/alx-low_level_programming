f77252500c7e7aa0a7cbadbab9b061e4671c029c
wac <wabaham9@gmail.com>
2022-07-26 06:05:47 +0000

    q3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,15 +26,15 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
-	       if(arr[i] == NULL)
-	       {	       
-		       for (i--; i >= 0; i--)
-		       {
-			       free(arr[i]);
-		       }
-		       free(arr);
-		       return (NULL);
-	       }		
+		if (arr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
 	}
 	for (j = 0; j < height; j++)
 	{
