3221b23317c172203763115e6b687eec5b0c45e9
wac <wabaham9@gmail.com>
2022-08-01 17:42:02 +0000

    2  and 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,19 @@ void print_dog(struct dog *d)
 	else if (d->name == NULL)
 	{
 		printf("Name: (nil)");
+		printf("Age: %.6f\n",  d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 	else if (d->age == 0)
 	{
+		printf("Name: %s\n", d->name);
 		printf("Age: (nil)");
+		printf("Owner: %s\n", d->owner);
 	}
 	else if (d->owner == NULL)
 	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %.6f\n",  d->age);
 		printf("Owner: (nil)");
 	}
 	else
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _cp - copy string
+ * @str: string given
+ *
+ * Return: pointer to new string
+ */
+
+char *_cp(char *str)
+{
+	int i, len = 0;
+	char *new;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	i = 0;
+	new = malloc(sizeof(char) * len + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -12,12 +47,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd = malloc(sizeof(dog_t));
+	char *nname, *nowner;
+
 	if (newd == NULL)
 	{
 		return (NULL);
 	}
-	newd->name = name;
+	else if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	nname = _cp(name);
+	nowner = _cp(owner);
+	newd->name = nname;
 	newd->age = age;
-	newd->owner = owner;
+	newd->owner = nowner;
 	return (newd);
 }
