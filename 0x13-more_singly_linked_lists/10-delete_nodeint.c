4220fcd4f13bd42ea02d83fe2ed2b645672bed43
wac <wabaham9@gmail.com>
2022-08-16 12:20:10 +0000

    9-10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		else
 		{
-			for (i = 0; i < index; i++)
+			for (i = 0; i < index - 1; i++)
 			{
 				if (cur != NULL)
 				{
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		tmp = *head;
-		while (i < idx)
+		while (i < idx - 1)
 		{
 			if (tmp != NULL)
 			{
