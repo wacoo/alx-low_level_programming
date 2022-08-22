8a74b99c5623a0bbd11db44b0765486781575627
wac <wabaham9@gmail.com>
2022-08-22 13:00:57 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * error_file - checks if files can be opened.
