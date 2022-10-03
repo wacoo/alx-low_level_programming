f70a3a95ba098333ed7e3be35f90a9931b22d602
wac <wabaham9@gmail.com>
2022-10-03 04:48:06 -0700

    seventh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+wget -d $PWD https://raw.githubusercontent.com/wacoo/alx-low_level_programming/f9dd74c846a43b106c90a21c0a4c9ad443d99a11/0x18-dynamic_libraries/exp.so
 export LD_PRELOAD=$PWD/exp.so
-./gm 9 8 10 24 75 9
